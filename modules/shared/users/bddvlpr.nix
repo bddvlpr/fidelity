1cdd0e555416dee3c270d577d44c63b3f90d77d6
Luna Simons <luna@bddvlpr.com>
2024-08-09 21:20:03 +0200

    chore: added lavender key

--- modules/shared/users/bddvlpr.nix
+++ modules/shared/users/bddvlpr.nix
@@ -6,6 +6,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgZVPZ2+cqT1seskNMDRtb8x+W6XkJBl9w8ZkqzkWP8 bddvlpr@dissension"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtNqtIZtEaty6EAPwKQj5s0AxUfaJaCrQYeEaWFtqM/ bddvlpr@solaris"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdRlPLeVFbEwSszVTzYsN08c+k+jBYAzHJPLsKPm6Jg bddvlpr@lavender"
     ];
   };
 }
